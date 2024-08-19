abf4e438b94425cbbd164903b875a37222725652
ABHISHEK K <abishkpk@gmail.com>
2024-08-19 16:41:23 +0530

    logoNew

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from './Header';
+import template1 from '../assets/template1.PNG';
+import template2 from '../assets/template2.PNG';
+import template3 from '../assets/template3.PNG';
+import template4 from '../assets/template4.PNG';
 
 function Home() {
   const navigate = useNavigate();
@@ -19,25 +23,25 @@ function Home() {
             className="cursor-pointer p-2 bg-white shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:scale-105"
             onClick={() => handleTemplateClick('template1')}
           >
-            <img src="./src/assets/template1.png" alt="Template 1" className="w-full h-full object-cover mb-4" />
+            <img src={template1} alt="Template 1" className="w-full h-full object-cover mb-4" />
           </div>
           <div
             className="cursor-pointer p-2 bg-white shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:scale-105"
             onClick={() => handleTemplateClick('template2')}
           >
-            <img src="./src/assets/template2.png" alt="Template 2" className="w-full h-full object-cover mb-4" />
+            <img src={template2} alt="Template 2" className="w-full h-full object-cover mb-4" />
           </div>
           <div
             className="cursor-pointer p-2 bg-white shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:scale-105"
             onClick={() => handleTemplateClick('template3')}
           >
-            <img src="./src/assets/template3.png" alt="Template 3" className="w-full h-full object-cover" />
+            <img src={template3} alt="Template 3" className="w-full h-full object-cover" />
           </div>
           <div
             className="cursor-pointer p-2 bg-white shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:scale-105"
             onClick={() => handleTemplateClick('template4')}
           >
-            <img src="./src/assets/template4.png" alt="Template 3" className="w-full h-full object-cover" />
+            <img src={template4} alt="Template 3" className="w-full h-full object-cover" />
           </div>
         </div>
       </div>
